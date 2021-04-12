4a63fece0843961e614f02893e345f24903c8985
Klubnika2009 <conflictboy@hotmail.cz>
2021-04-12 22:35:42 +0200

    wepp

--- gulp/tasks/webp.js
+++ gulp/tasks/webp.js
@@ -3,7 +3,6 @@
 module.exports = () => {
   $.gulp.task('webp', () => {
     return $.gulp.src($.path.webp.src)
-      .pipe($.newer($.path.webp.build))
       .pipe($.webp())
       .pipe($.debug({title: 'webp:'}))
       .pipe($.gulp.dest($.path.webp.build))
